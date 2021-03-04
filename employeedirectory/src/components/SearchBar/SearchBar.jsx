ff99f342c1661732aa823c3588b44574d319c048
Christian Goldman <christian.goldman9638@gmail.com>
2021-03-04 14:52:09 -0600

    updating page based upon search results

--- employeedirectory/src/components/SearchBar/SearchBar.jsx
+++ employeedirectory/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {useState} from "react";
 
-function SearchBar() {
+function SearchBar(employees) {
+  const [userInput, setUserInput] = useState("");
+  const handleChange = (e) => {
+    const value = setUserInput(e.target.defaultValue);
+    console.log(value);
+    employees = value;
+  }
+
   return (
     <div className="search">
       <form className="form">
-        <input className="bar" type="search" placeholder="Search" aria-label="Search">
+        <input className="bar" type="search" placeholder="Search" aria-label="Search" onChange={handleChange} value={userInput}>
         </input>
-        <button className="searchBtn" type="button">Search</button>
+        <button onClick={handleChange} className="searchBtn" type="button">Search</button>
       </form>
     </div>
   )
